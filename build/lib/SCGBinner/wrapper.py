f91dfe1b99d6ba9718c20d99926dbf20e08c2f5f
Hans <49517797+htaohan@users.noreply.github.com>
2026-02-15 22:50:40 +0000

    Delete build/lib/SCGBinner directory

--- build/lib/SCGBinner/wrapper.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-import os
-import sys
-
-def main():
-    script_path = os.path.join(os.path.dirname(__file__), "run_scgbinner.sh")
-
-    subprocess.run([script_path] + sys.argv[1:])
